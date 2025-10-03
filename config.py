1dd4357bb8386afed91832506c6cf85d2ea905d2
shashwath9148 <123259834+shashwath9148@users.noreply.github.com>
2025-10-03 09:39:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,21 +7,21 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "28071110")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "3adf9a220b6debbb23cb8b4a4418d704")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7640881691:AAGFOeF6wmBDv1X4c8bXp1CRrq6lhcQnv5o") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "VJ_Botz") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "Shashu9148") 
 
-             # Don't Remove Credit @VJ_Botz
-             # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-             # Ask Doubt on telegram @KingVJ01
+             # Don't Remove Credit @Shashu9148
+             # Subscribe YouTube Channel For Amazing Bot @shashu9148
+             # Ask Doubt on telegram @ShaNaAdminConnect_Bot
 
 DB_NAME = os.environ.get("DB_NAME", "renamevjbot")     
 
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://shashwathgondi9148:Shashwath@9148@cluster0.edy6amv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
